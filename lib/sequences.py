b8381ba4ec73b963e63c3f9aacb0f58e3fd54ea7
Fatuma Ibrahim <fatmazahraabdulrashid@gmail.com>
2024-09-04 22:08:13 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
 
-#"def print_fibonacci(length):
-   # '''Prints the first 'length' numbers in the Fibonacci sequence.'''
-   # if length == 0:
-      #  print('[]')
-       # return
-    #fib_sequence = [0, 1]
-    #while len(fib_sequence) < length:
-        #fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
-
 def print_fibonacci(length):
+    
     if length <= 0:
         print([])
         return
